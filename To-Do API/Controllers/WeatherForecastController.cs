8f9f2e3b1bbb3a123c3c6677ecad7739d8d8a903
ConanJian <conanjian2@gmail.com>
2022-03-27 16:49:54 -0700

    Finished Depency Injection for Custom Configuration. Currently Working on Routing for APIs

--- To-Do API/Controllers/WeatherForecastController.cs
+++ To-Do API/Controllers/WeatherForecastController.cs
@@ -9,25 +9,37 @@ namespace To_Do_API.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _config;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        //How does dependency injector know which IConfiguration to Inject?
+        //Isn't the default IConfiguration still a thing? Doesn't seem like it or maybe it got removed?
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //[Route("~/GetWeather")]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+        [HttpGet(Name = "GetRandomText")]
+        //[Route("/GetText")]
+        public string GetText()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return _config.GetRequiredSection("RandomText").GetValue<string>("Text1");
+            //return _config.GetValue<string>("Test");
         }
     }
 }
--- To-Do API/Program.cs
+++ To-Do API/Program.cs
@@ -1,11 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Create Custom Configuration
 
+string configFilePath = ".\\devSettings.json";
+IConfiguration config = new ConfigurationBuilder().AddJsonFile(configFilePath)
+    .Build();
+
+string valueInConfigFile = config.GetRequiredSection("RandomText").GetValue<string>("Text1");
+//string baseDirectory = AppContext.BaseDirectory;
+//Console.WriteLine(valueInConfigFile + "\n" + baseDirectory);
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton(config);
 
 var app = builder.Build();
 
