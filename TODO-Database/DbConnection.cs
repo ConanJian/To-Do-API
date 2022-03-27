264f41a4aa93d19d7be0ca91fe570a6f3c0c2e1a
ConanJian <conanjian2@gmail.com>
2022-03-26 19:02:48 -0700

    Add project files.

---
+++ TODO-Database/DbConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DatabaseConnection
+{
+    internal class DbConnection
+    {
+        private readonly SqlConnection connection;
+        public DbConnection(string connectionString)
+        { 
+            connection = new SqlConnection(connectionString);
+        }
+        public DataSet ReadData(string sqlQuery)
+        { 
+            
+        }
+        public void WriteData(string sqlQuery)
+        { 
+            
+        }
+    }
+}
+
+/*
+    add dependency injection for connection
+    get connection string from configuration file
+    
+ */
