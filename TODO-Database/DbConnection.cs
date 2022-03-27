0954aa9ff3fa2a8d0a1c1543e0a0aeb03d48ef03
ConanJian <conanjian2@gmail.com>
2022-03-26 20:16:18 -0700

    Added Database Connection

--- TODO-Database/DbConnection.cs
+++ TODO-Database/DbConnection.cs
@@ -4,20 +4,35 @@ using System.Data.SqlClient;
 
 namespace DatabaseConnection
 {
-    internal class DbConnection
+    internal class DbConnection: IDisposable
     {
-        private readonly SqlConnection connection;
+        private readonly SqlConnection _connection;
         public DbConnection(string connectionString)
         { 
-            connection = new SqlConnection(connectionString);
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
         }
         public DataSet ReadData(string sqlQuery)
-        { 
-            
+        {
+            DataSet result = new DataSet();
+            SqlDataAdapter adpater = new SqlDataAdapter();
+            SqlCommand sqlCommand = new SqlCommand(sqlQuery, _connection);
+            adpater.SelectCommand = sqlCommand;
+            //adpater.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            adpater.Fill(result);
+            return result;
         }
-        public void WriteData(string sqlQuery)
-        { 
-            
+        public void ModifyData(string sqlQuery)
+        {
+            using (SqlCommand sqlCommand = new SqlCommand(sqlQuery, _connection))
+            {
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+        public void Dispose()
+        {
+            _connection.Dispose();
         }
     }
 }
