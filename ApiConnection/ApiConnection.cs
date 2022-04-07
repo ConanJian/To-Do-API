fcc09ac9460e3c95fe6ec0e081ac40fc0dd6613c
ConanJian <conanjian2@gmail.com>
2022-04-06 23:16:30 -0700

    Created Plan for next time I work on the project.

---
+++ ApiConnection/ApiConnection.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ApiConnection
+{
+    public class ApiConnection
+    {
+
+    }
+}
--- ToDoTerminal/Program.cs
+++ ToDoTerminal/Program.cs
@@ -7,6 +7,10 @@ namespace ToDoTerminal
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //Probably will write macros here
+            //Will make a library to actually communicate with my Api
+            //This console will use those libraries to A) provide a user experience
+            //B) Will use this console for macros
         }
     }
 }
